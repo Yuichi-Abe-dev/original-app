079448b56865f76f624d392b20315b9709c50edc
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-02-11 17:58:39 +0900

    ユーザー詳細ページでのユーザーの情報の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   def show
     #uset_idから特定のレコードを取得
     @user = User.find(params[:id])
+    @prototype = Prototype.new
+    @prototypes = @user.prototypes.includes(:user)
   end
 
 end
