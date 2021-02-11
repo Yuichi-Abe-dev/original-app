d205064aa2782912eba7f0f158c2f8ca3d3ddacc
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-02-11 21:31:52 +0900

    FactoryBotの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    name {Faker::Name.name}
+    profile {Faker::Company.industry}
+    occupation {Faker::Company.profession}
+    position {Faker::Construction.role}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
     password_confirmation {password}
   end
 end
